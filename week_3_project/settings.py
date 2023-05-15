546b2be2d1981b693400ff754d8c744dba67ad31
X_Trial_Test <1trialandtest@gmail.com>
2023-05-15 21:03:12 +0300

    installation

--- week_3_project/settings.py
+++ week_3_project/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sma',
+    "django_bootstrap5",
+    
 ]
 
 MIDDLEWARE = [
